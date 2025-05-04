03009785f55e50a5ec9e28fbf891376a184ae90b
Anjali <anjali.parm@gmail.com>
2025-05-04 14:00:59 +0100

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,23 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Marseille", "Paris", "Luton", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the capital of Italy?",
+        "choices": ["Venice", "Austria", "Rome", "Narnia"],
+        "answer": "Rome"
+    },
+    {
+        "question": "What is the capital of Japan?",
+        "choices": ["Kyoto", "Toyota", "Taipei", "Tokyo"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "What is the capital of the Philippines?",
+        "choices": ["Singapore", "Manila", "Vanilla", "Quezon"],
+        "answer": "Manila"
+    }
+    # Add more questions here
+]
